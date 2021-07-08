a9421deea3bada6aadb5d8c68e954e33ce8459c1
sb17ho <sb17ho@brocku.ca>
2021-07-08 10:49:05 -0400

    Selection sort test passed, visualization in progress

--- src/sortingalgorithms/SelectionSort.java
+++ src/sortingalgorithms/SelectionSort.java
@@ -24,7 +24,6 @@ public class SelectionSort implements Sorter {
 
         TranslateTransition a, b;
 
-        //Okay, using transition than equating set height
         for (int i = 0; i < array.length; i++) {
             int minValIndex = findMin(i, array.length, array)[1];
             Bar temp = array[i];
@@ -33,30 +32,4 @@ public class SelectionSort implements Sorter {
         }
     }
 }
-
-//TODO: Compare using the height directly to avoid the seHeight Issue
-
-//        Integer[] unsorted = new Integer[array.length];
-//
-//        for (int i = 0; i < array.length; i++) {
-//            unsorted[i] = (int) array[i].getHeight();
-//        }
-//
-//        Integer[] sorted = sort(unsorted);
-//
-//        for (int i = 0; i < sorted.length; i++) {
-//            array[i].setHeight(sorted[i]);
-//        }
-
-//    private void sort(Integer[] unsorted) {
-//        TranslateTransition a, b;
-//
-//        for (int i = 0; i < unsorted.length; i++) {
-//            int[] minVal = findMin(i, unsorted.length, unsorted);
-//            int temp = unsorted[i];
-//            unsorted[i] = minVal[0];
-//            unsorted[minVal[1]] = temp;
-//        }
-//    }
-
 
